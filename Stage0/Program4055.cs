c518d8ed411702c726bcb6fa7a81110cb8113693
Leah <leahsh1093@gmail.com>
2023-10-11 00:57:32 +0300

    initial code+Welcome functions

---
+++ Stage0/Program4055.cs
@@ -0,0 +1,26 @@
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");*/
+using System;
+
+namespace Stage0 // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4055();
+            Welcome3394();
+            Console.ReadKey();
+
+        }
+
+        static partial void Welcome3394();
+        private static void Welcome4055()
+        {
+            Console.Write("Enter your name: ");
+            string user_name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user_name);
+            /*    Console.Write("press any key to continue...");*/
+        }
+    }
+}
