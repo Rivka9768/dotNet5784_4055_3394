6e1efacb009d6fca07902412610f909c875d0221
Rivka <sorsher@acad.jct.ac.il>
2023-11-12 15:56:15 +0200

    up to 9

--- DalFacade/Class1.cs
+++ DalFacade/Class1.cs
@@ -2,13 +2,6 @@
 {
     public class Class1
     {
-        //לבדוק מה NULLABLE בתכונות של הישויות
-        //לעשות את ה DATETIME
-        //לעשות NULLABLE ב DATA SORECE
-        //לעשות NULLABLE ב DATA SORECE
 
-
-
-    //ךבדוק ולגמור ENUM
     }
 }
