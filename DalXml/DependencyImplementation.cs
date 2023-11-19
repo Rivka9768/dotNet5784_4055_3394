9200c7209601a512a62c400837c478360f898a94
Leah <leahsh1093@gmail.com>
2023-11-19 17:32:04 +0200

    שלב 3 פרק 5

--- DalXml/DependencyImplementation.cs
+++ DalXml/DependencyImplementation.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Dal
+using DalApi;
+using DO;
+
+namespace Dal;
+
+internal class DependencyImplementation : IDependency
 {
-    internal class DependencyImplementation
+    public int Create(Dependency item)
     {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(Func<Dependency, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Dependency item)
+    {
+        throw new NotImplementedException();
     }
 }
