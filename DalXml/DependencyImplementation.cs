8faff9d23dbe34e9a0b1e3d9df9742f906905ef2
Rivka <sorsher@acad.jct.ac.il>
2023-11-21 15:14:55 +0200

    שלב 5

--- DalXml/DependencyImplementation.cs
+++ DalXml/DependencyImplementation.cs
@@ -31,21 +31,46 @@ internal class DependencyImplementation : IDependency
 
     public Dependency? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Dependency> dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencys");
+        Dependency dependency = (from d in dependencies
+                                 let dId = d.Id
+                                 where dId == id
+                                 select d).FirstOrDefault()!;
+        return dependency;
     }
 
     public Dependency? Read(Func<Dependency, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Dependency> dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencys");
+        return (from item in dependencies
+                where filter(item)
+                select item).FirstOrDefault();
     }
 
     public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<Dependency> dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencys");
+        if (filter != null)
+        {
+            return from item in dependencies
+                   where filter(item)
+                   select item;
+        }
+        return from item in dependencies
+               select item;
     }
 
     public void Update(Dependency item)
     {
-        throw new NotImplementedException();
+        List<Dependency> dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>("dependencys");
+        Dependency dependency = (from d in dependencies
+                                 let dId = d.Id
+                                 where dId == item.Id
+                                 select d).FirstOrDefault()!;
+        if (dependency == null)
+            throw new DalDoesNotExistException($"Dependency with ID={item.Id} does not exists");
+        dependencies.Remove(dependency);
+        dependencies.Add(item);
+        XMLTools.SaveListToXMLSerializer<Dependency>(dependencies, "dependencys");
     }
 }
